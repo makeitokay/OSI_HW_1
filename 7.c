7f9876cedb7c05cd3fb41e79b36e4ddea6edca15
makeitokay <andrey.mu11@yandex.ru>
2023-03-19 21:17:24 +0300

    7

---
+++ 7.c
@@ -0,0 +1,69 @@
+#include "reverse.h"
+#include "utils.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <stdlib.h>
+
+const int SIZE = 5000;
+
+int main(int argc, char** argv) {
+    char buffer[SIZE];
+
+    char first_fifo_name[] = "7_1.fifo";
+    char second_fifo_name[] = "7_2.fifo";
+
+    createFifo(first_fifo_name);
+    createFifo(second_fifo_name);
+
+    int chpid = fork();
+    if (chpid == -1) {
+        printf("Не удалось создать дочерний процесс от 1-го процесса.\n");
+        exit(-1);
+    } else if (chpid == 0) {
+        printf("Второй процесс: считываем данные из первого канала и проводим обработку данных.\n");
+
+        int first_fd = getReadonlyFifo(first_fifo_name);
+
+        readFromFd(first_fd, buffer, SIZE);
+        reverse(buffer);
+
+        int second_fd = getWriteonlyFifo(second_fifo_name);
+
+        writeToFd(second_fd, buffer, SIZE);
+
+        closeFd(first_fd);
+        closeFd(second_fd);
+
+        printf("Второй процесс: данные считаны, обработаны и записаны во второй канал.\n");
+
+    } else {
+        printf("Первый процесс: считываем входные данные из файла и отправляем в первый канал.\n");
+        char* input_filename = argv[1];
+        readFromFile(input_filename, buffer);
+
+        int first_fd = getWriteonlyFifo(first_fifo_name);
+
+        writeToFd(first_fd, buffer, SIZE);
+
+        printf("Первый процесс: данные из входного файла считаны и записаны в первый канал.\n");
+
+        char second_buffer[SIZE];
+
+        printf("Первый процесс: считываем данные из второго канала и записываем в файл.\n");
+
+        int second_fd = getReadonlyFifo(second_fifo_name);
+
+        readFromFd(second_fd, second_buffer, SIZE);
+
+        char* output_filename = argv[2];
+        writeToFile(output_filename, second_buffer);
+
+        printf("Первый процесс: данные из второго канала считаны и записаны в файл.\n");
+
+        closeFd(first_fd);
+        closeFd(second_fd);
+    }
+
+    return 0;
+}
